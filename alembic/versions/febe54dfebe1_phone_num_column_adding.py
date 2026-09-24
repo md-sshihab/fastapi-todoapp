d39c96c3e94db1d9014079c57b71d0e32519f4f9
Md. Sofiullah Shihab <mdsofiullahshihab@gmail.com>
2026-09-24 14:17:59 +0600

    first commit

---
+++ alembic/versions/febe54dfebe1_phone_num_column_adding.py
@@ -0,0 +1,27 @@
+"""phone num column adding
+
+Revision ID: febe54dfebe1
+Revises: 
+Create Date: 2026-09-24 10:38:53.471942
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'febe54dfebe1'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column('users',sa.Column('phone',sa.String(),nullable=True))
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
