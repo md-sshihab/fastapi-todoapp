eeca550aaaf258f3c323356ec167e4d4a4c5c080
Md. Sofiullah Shihab <mdsofiullahshihab@gmail.com>
2026-09-24 20:36:47 +0600

    Fix database configuration

--- alembic/versions/febe54dfebe1_phone_num_column_adding.py
+++ alembic/versions/febe54dfebe1_phone_num_column_adding.py
@@ -19,7 +19,8 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column('users',sa.Column('phone',sa.String(),nullable=True))
+    #op.add_column('users',sa.Column('phone',sa.String(),nullable=True))
+    pass
 
 
 def downgrade() -> None:
